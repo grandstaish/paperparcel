1d8bac40064226a04d1fbc880ff42fac00bbcd42
Brad Campbell <grandstaish@gmail.com>
2016-01-12 17:41:28 +1300

    Double property tests

---
+++ dataparcel-compiler/src/test/java/nz/bradcampbell/dataparcel/DoublePropertyTests.java
@@ -0,0 +1,209 @@
+package nz.bradcampbell.dataparcel;
+
+import com.google.common.base.Joiner;
+import com.google.testing.compile.JavaFileObjects;
+import org.junit.Test;
+
+import javax.tools.JavaFileObject;
+
+import static com.google.common.truth.Truth.assertAbout;
+import static com.google.testing.compile.JavaSourceSubjectFactory.javaSource;
+
+public class DoublePropertyTests {
+
+  @Test public void nullableDoubleTest() throws Exception {
+    JavaFileObject source = JavaFileObjects.forSourceString("test.Test", Joiner.on('\n').join(
+        "package test;",
+        "import android.support.annotation.Nullable;",
+        "import nz.bradcampbell.dataparcel.DataParcel;",
+        "@DataParcel",
+        "public final class Test {",
+        "@Nullable private final Double child;",
+        "public Test(@Nullable Double child) {",
+        "this.child = child;",
+        "}",
+        "@Nullable public Double component1() {",
+        "return this.child;",
+        "}",
+        "}"
+    ));
+
+    JavaFileObject expectedSource = JavaFileObjects.forSourceString("test/TestParcel", Joiner.on('\n').join(
+        "package test;",
+        "import android.os.Parcel;",
+        "import android.os.Parcelable;",
+        "import java.lang.Double;",
+        "import java.lang.Override;",
+        "public class TestParcel implements Parcelable {",
+        "public static final Parcelable.Creator<TestParcel> CREATOR = new Parcelable.Creator<TestParcel>() {",
+        "@Override public TestParcel createFromParcel(Parcel in) {",
+        "return new TestParcel(in);",
+        "}",
+        "@Override public TestParcel[] newArray(int size) {",
+        "return new TestParcel[size];",
+        "}",
+        "};",
+        "private final Test data;",
+        "private TestParcel(Test data) {",
+        "this.data = data;",
+        "}",
+        "private TestParcel(Parcel in) {",
+        "Double component1 = null;",
+        "if (in.readInt() == 0) {",
+        "component1 = in.readDouble();",
+        "}",
+        "this.data = new Test(component1);",
+        "}",
+        "public static final TestParcel wrap(Test data) {",
+        "return new TestParcel(data);",
+        "}",
+        "public Test getContents() {",
+        "return data;",
+        "}",
+        "@Override public int describeContents() {",
+        "return 0;",
+        "}",
+        "@Override public void writeToParcel(Parcel dest, int flags) {",
+        "if (data.component1() == null) {",
+        "dest.writeInt(1);",
+        "} else {",
+        "dest.writeInt(0);",
+        "Double component1 = data.component1();",
+        "dest.writeDouble(component1);",
+        "}",
+        "}",
+        "}"
+    ));
+
+    assertAbout(javaSource()).that(source)
+        .processedWith(new DataParcelProcessor())
+        .compilesWithoutError()
+        .and()
+        .generatesSources(expectedSource);
+  }
+
+  @Test public void doubleTest() throws Exception {
+    JavaFileObject source = JavaFileObjects.forSourceString("test.Test", Joiner.on('\n').join(
+        "package test;",
+        "import nz.bradcampbell.dataparcel.DataParcel;",
+        "@DataParcel",
+        "public final class Test {",
+        "private final Double child;",
+        "public Test(Double child) {",
+        "this.child = child;",
+        "}",
+        "public Double component1() {",
+        "return this.child;",
+        "}",
+        "}"
+    ));
+
+    JavaFileObject expectedSource = JavaFileObjects.forSourceString("test/TestParcel", Joiner.on('\n').join(
+        "package test;",
+        "import android.os.Parcel;",
+        "import android.os.Parcelable;",
+        "import java.lang.Double;",
+        "import java.lang.Override;",
+        "public class TestParcel implements Parcelable {",
+        "public static final Parcelable.Creator<TestParcel> CREATOR = new Parcelable.Creator<TestParcel>() {",
+        "@Override public TestParcel createFromParcel(Parcel in) {",
+        "return new TestParcel(in);",
+        "}",
+        "@Override public TestParcel[] newArray(int size) {",
+        "return new TestParcel[size];",
+        "}",
+        "};",
+        "private final Test data;",
+        "private TestParcel(Test data) {",
+        "this.data = data;",
+        "}",
+        "private TestParcel(Parcel in) {",
+        "Double component1 = null;",
+        "component1 = in.readDouble();",
+        "this.data = new Test(component1);",
+        "}",
+        "public static final TestParcel wrap(Test data) {",
+        "return new TestParcel(data);",
+        "}",
+        "public Test getContents() {",
+        "return data;",
+        "}",
+        "@Override public int describeContents() {",
+        "return 0;",
+        "}",
+        "@Override public void writeToParcel(Parcel dest, int flags) {",
+        "Double component1 = data.component1();",
+        "dest.writeDouble(component1);",
+        "}",
+        "}"
+    ));
+
+    assertAbout(javaSource()).that(source)
+        .processedWith(new DataParcelProcessor())
+        .compilesWithoutError()
+        .and()
+        .generatesSources(expectedSource);
+  }
+
+  @Test public void primitiveDoubleTest() throws Exception {
+    JavaFileObject source = JavaFileObjects.forSourceString("test.Test", Joiner.on('\n').join(
+        "package test;",
+        "import nz.bradcampbell.dataparcel.DataParcel;",
+        "@DataParcel",
+        "public final class Test {",
+        "private final double child;",
+        "public Test(double child) {",
+        "this.child = child;",
+        "}",
+        "public double component1() {",
+        "return this.child;",
+        "}",
+        "}"
+    ));
+
+    JavaFileObject expectedSource = JavaFileObjects.forSourceString("test/TestParcel", Joiner.on('\n').join(
+        "package test;",
+        "import android.os.Parcel;",
+        "import android.os.Parcelable;",
+        "import java.lang.Override;",
+        "public class TestParcel implements Parcelable {",
+        "public static final Parcelable.Creator<TestParcel> CREATOR = new Parcelable.Creator<TestParcel>() {",
+        "@Override public TestParcel createFromParcel(Parcel in) {",
+        "return new TestParcel(in);",
+        "}",
+        "@Override public TestParcel[] newArray(int size) {",
+        "return new TestParcel[size];",
+        "}",
+        "};",
+        "private final Test data;",
+        "private TestParcel(Test data) {",
+        "this.data = data;",
+        "}",
+        "private TestParcel(Parcel in) {",
+        "double component1;",
+        "component1 = in.readDouble();",
+        "this.data = new Test(component1);",
+        "}",
+        "public static final TestParcel wrap(Test data) {",
+        "return new TestParcel(data);",
+        "}",
+        "public Test getContents() {",
+        "return data;",
+        "}",
+        "@Override public int describeContents() {",
+        "return 0;",
+        "}",
+        "@Override public void writeToParcel(Parcel dest, int flags) {",
+        "double component1 = data.component1();",
+        "dest.writeDouble(component1);",
+        "}",
+        "}"
+    ));
+
+    assertAbout(javaSource()).that(source)
+        .processedWith(new DataParcelProcessor())
+        .compilesWithoutError()
+        .and()
+        .generatesSources(expectedSource);
+  }
+}
