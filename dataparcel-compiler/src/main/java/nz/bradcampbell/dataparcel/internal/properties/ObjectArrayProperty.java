bc98bc5d6bd9fa004627d0f1e3bcdba5ee37558f
Brad Campbell <grandstaish@gmail.com>
2016-01-15 19:12:07 +1300

    Add support for generic arrays

--- dataparcel-compiler/src/main/java/nz/bradcampbell/dataparcel/internal/properties/ObjectArrayProperty.java
+++ dataparcel-compiler/src/main/java/nz/bradcampbell/dataparcel/internal/properties/ObjectArrayProperty.java
@@ -1,6 +1,7 @@
 package nz.bradcampbell.dataparcel.internal.properties;
 
 import com.squareup.javapoet.*;
+import nz.bradcampbell.dataparcel.internal.Properties;
 import nz.bradcampbell.dataparcel.internal.Property;
 
 public class ObjectArrayProperty extends Property {
@@ -10,19 +11,69 @@ public class ObjectArrayProperty extends Property {
 
   @Override protected void readFromParcelInner(CodeBlock.Builder block, ParameterSpec in) {
     TypeName objectArrayClassName = ArrayTypeName.get(Object[].class);
-    TypeName componentType = ((ArrayTypeName) getPropertyType().getWrappedRawTypeName()).componentType;
     String objectArrayName = getWrappedName();
-    block.addStatement("$T $N = $N.readArray($T.class.getClassLoader())", objectArrayClassName, objectArrayName, in,
-        componentType);
+    block.addStatement("$T $N = $N.readArray(getClass().getClassLoader())", objectArrayClassName, objectArrayName, in);
+    unparcelVariable(block);
+  }
+
+  @Override public void unparcelVariable(CodeBlock.Builder block) {
+    Property.Type propertyType = getPropertyType();
+
+    Property.Type componentPropertyType = propertyType.getChildType(0);
+    TypeName componentType = componentPropertyType.getTypeName(false);
+    TypeName wrappedComponentType = componentPropertyType.getWrappedTypeName();
+    TypeName rawComponentType = componentPropertyType.getRawTypeName();
+
     String variableName = getName();
-    block.addStatement("$N = new $T[$N.length]", variableName, componentType, objectArrayName);
+    String wrappedVariableName = getWrappedName();
+
+    block.addStatement("$N = new $T[$N.length]", variableName, rawComponentType, wrappedVariableName);
+
     String indexName = getName() + "Index";
-    block.beginControlFlow("for (int $N = 0; $N < $N.length; $N++)", indexName, indexName, objectArrayName, indexName);
-    block.addStatement("$N[$N] = ($T) $N[$N]", variableName, indexName, componentType, objectArrayName, indexName);
+    block.beginControlFlow("for (int $N = 0; $N < $N.length; $N++)", indexName, indexName, wrappedVariableName, indexName);
+    
+    String innerName = "_" + variableName;
+    String innerWrappedName = "_" + wrappedVariableName;
+    block.addStatement("$T $N = null", componentType, innerName);
+    block.addStatement("$T $N = ($T) $N[$N]", wrappedComponentType, innerWrappedName, wrappedComponentType, wrappedVariableName, indexName);
+    Properties.createProperty(componentPropertyType, false, innerName).unparcelVariable(block);
+    block.addStatement("$N[$N] = $N", variableName, indexName, innerName);
+
     block.endControlFlow();
   }
 
   @Override protected void writeToParcelInner(CodeBlock.Builder block, ParameterSpec dest, String variableName) {
     block.addStatement("$N.writeArray($N)", dest, variableName);
+  }
+
+  @Override public String generateParcelableVariable(CodeBlock.Builder block, String source, boolean includeWildcards) {
+    Property.Type propertyType = getPropertyType();
+    String variableName = super.generateParcelableVariable(block, source, includeWildcards);
+
+    if (!propertyType.isParcelable()) {
+
+      TypeName wrappedTypeName = propertyType.getWrappedTypeName();
+      TypeName wrappedComponentRawTypeName = propertyType.getChildType(0).getWrappedRawTypeName();
+      String wrappedVariableName = getWrappedName();
+
+      block.addStatement("$T $N = new $T[$N.length]", wrappedTypeName, wrappedVariableName,
+          wrappedComponentRawTypeName, variableName);
+
+      String indexName = variableName + "Index";
+      block.beginControlFlow("for (int $N = 0; $N < $N.length; $N++)", indexName, indexName, variableName, indexName);
+
+      String innerName = "_" + variableName;
+      String innerSource = variableName + "[" + indexName + "]";
+      String wrappedInnerName = Properties.createProperty(propertyType.getChildType(0), false, innerName)
+          .generateParcelableVariable(block, innerSource, false);
+
+      block.addStatement("$N[$N] = $N", wrappedVariableName, indexName, wrappedInnerName);
+
+      block.endControlFlow();
+
+      variableName = wrappedVariableName;
+    }
+
+    return variableName;
   }
 }
--- dataparcel-compiler/src/test/java/nz/bradcampbell/dataparcel/ObjectArrayTests.java
+++ dataparcel-compiler/src/test/java/nz/bradcampbell/dataparcel/ObjectArrayTests.java
@@ -8,6 +8,8 @@ import javax.tools.JavaFileObject;
 
 import static com.google.common.truth.Truth.assertAbout;
 import static com.google.testing.compile.JavaSourceSubjectFactory.javaSource;
+import static com.google.testing.compile.JavaSourcesSubjectFactory.javaSources;
+import static java.util.Arrays.asList;
 
 public class ObjectArrayTests {
 
@@ -51,10 +53,13 @@ public class ObjectArrayTests {
         "private TestParcel(Parcel in) {",
         "Boolean[] component1 = null;",
         "if (in.readInt() == 0) {",
-        "Object[] component1Wrapped = in.readArray(Boolean.class.getClassLoader());",
+        "Object[] component1Wrapped = in.readArray(getClass().getClassLoader());",
         "component1 = new Boolean[component1Wrapped.length];",
         "for (int component1Index = 0; component1Index < component1Wrapped.length; component1Index++) {",
-        "component1[component1Index] = (Boolean) component1Wrapped[component1Index];",
+        "Boolean _component1 = null;",
+        "Boolean _component1Wrapped = (Boolean) component1Wrapped[component1Index];",
+        "_component1 = _component1Wrapped;",
+        "component1[component1Index] = _component1;",
         "}",
         "}",
         "this.data = new Test(component1);",
@@ -81,6 +86,109 @@ public class ObjectArrayTests {
     ));
 
     assertAbout(javaSource()).that(source)
+        .processedWith(new DataParcelProcessor())
+        .compilesWithoutError()
+        .and()
+        .generatesSources(expectedSource);
+  }
+
+  @Test public void genericNonParcelableArrayTest() {
+    JavaFileObject source = JavaFileObjects.forSourceString("test.Test", Joiner.on('\n').join(
+        "package test;",
+        "import nz.bradcampbell.dataparcel.DataParcel;",
+        "import android.util.SparseArray;",
+        "@DataParcel",
+        "public final class Test {",
+        "private final SparseArray<Child>[] child;",
+        "public Test(SparseArray<Child>[] child) {",
+        "this.child = child;",
+        "}",
+        "public SparseArray<Child>[] component1() {",
+        "return this.child;",
+        "}",
+        "}"
+    ));
+
+    JavaFileObject dataClassChild = JavaFileObjects.forSourceString("test.Child", Joiner.on('\n').join(
+        "package test;",
+        "public final class Child {",
+        "private final Integer test;",
+        "public Child(Integer test) {",
+        "this.test = test;",
+        "}",
+        "public Integer component1() {",
+        "return this.test;",
+        "}",
+        "}"
+    ));
+
+    JavaFileObject expectedSource = JavaFileObjects.forSourceString("test/TestParcel", Joiner.on('\n').join(
+        "package test;",
+        "import android.os.Parcel;",
+        "import android.os.Parcelable;",
+        "import android.util.SparseArray;",
+        "import java.lang.Object;",
+        "import java.lang.Override;",
+        "public class TestParcel implements Parcelable {",
+        "public static final Parcelable.Creator<TestParcel> CREATOR = new Parcelable.Creator<TestParcel>() {",
+        "@Override public TestParcel createFromParcel(Parcel in) {",
+        "return new TestParcel(in);",
+        "}",
+        "@Override public TestParcel[] newArray(int size) {",
+        "return new TestParcel[size];",
+        "}",
+        "};",
+        "private final Test data;",
+        "private TestParcel(Test data) {",
+        "this.data = data;",
+        "}",
+        "private TestParcel(Parcel in) {",
+        "SparseArray<Child>[] component1 = null;",
+        "Object[] component1Wrapped = in.readArray(getClass().getClassLoader());",
+        "component1 = new SparseArray[component1Wrapped.length];",
+        "for (int component1Index = 0; component1Index < component1Wrapped.length; component1Index++) {",
+        "SparseArray<Child> _component1 = null;",
+        "SparseArray<ChildParcel> _component1Wrapped = (SparseArray<ChildParcel>) component1Wrapped[component1Index];",
+        "_component1 = new SparseArray<Child>();",
+        "for (int _component1Index = 0; _component1Index < _component1Wrapped.size(); _component1Index++) {",
+        "Child __component1 = null;",
+        "int _component1WrappedKey = _component1Wrapped.keyAt(_component1Index);",
+        "ChildParcel __component1Wrapped = _component1Wrapped.get(_component1WrappedKey);",
+        "__component1 = __component1Wrapped.getContents();",
+        "_component1.put(_component1WrappedKey, __component1);",
+        "}",
+        "component1[component1Index] = _component1;",
+        "}",
+        "this.data = new Test(component1);",
+        "}",
+        "public static final TestParcel wrap(Test data) {",
+        "return new TestParcel(data);",
+        "}",
+        "public Test getContents() {",
+        "return data;",
+        "}",
+        "@Override public int describeContents() {",
+        "return 0;",
+        "}",
+        "@Override public void writeToParcel(Parcel dest, int flags) {",
+        "SparseArray<Child>[] component1 = data.component1();",
+        "SparseArray<ChildParcel>[] component1Wrapped = new SparseArray[component1.length];",
+        "for (int component1Index = 0; component1Index < component1.length; component1Index++) {",
+        "SparseArray<Child> _component1 = component1[component1Index];",
+        "SparseArray<ChildParcel> _component1Wrapped = new SparseArray<ChildParcel>();",
+        "for (int _component1Index = 0; _component1Index < _component1.size(); _component1Index++) {",
+        "int _component1Key = _component1.keyAt(_component1Index);",
+        "ChildParcel __component1 = ChildParcel.wrap(_component1.get(_component1Key));",
+        "_component1Wrapped.put(_component1Key, __component1);",
+        "}",
+        "component1Wrapped[component1Index] = _component1Wrapped;",
+        "}",
+        "dest.writeArray(component1Wrapped);",
+        "}",
+        "}"
+    ));
+
+    assertAbout(javaSources()).that(asList(source, dataClassChild))
         .processedWith(new DataParcelProcessor())
         .compilesWithoutError()
         .and()
