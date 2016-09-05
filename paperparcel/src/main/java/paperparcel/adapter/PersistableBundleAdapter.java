1b1a9d18f8b19d4c57d22baaff474fe28394ef83
Brad Campbell <grandstaish@gmail.com>
2016-09-05 21:03:51 +1200

    Prioritise custom adapters over built-in ones and process most common adapter types first.

---
+++ paperparcel/src/main/java/paperparcel/adapter/PersistableBundleAdapter.java
@@ -0,0 +1,24 @@
+package paperparcel.adapter;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Parcel;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import paperparcel.TypeAdapter;
+
+/** Default {@link TypeAdapter} for {@link PersistableBundle} types */
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+public final class PersistableBundleAdapter extends AbstractAdapter<PersistableBundle> {
+  public static final PersistableBundleAdapter INSTANCE = new PersistableBundleAdapter();
+
+  @NonNull @Override protected PersistableBundle read(@NonNull Parcel source) {
+    return source.readPersistableBundle(getClass().getClassLoader());
+  }
+
+  @Override protected void write(@NonNull PersistableBundle value, @NonNull Parcel dest, int flags) {
+    dest.writePersistableBundle(value);
+  }
+
+  private PersistableBundleAdapter() {}
+}
