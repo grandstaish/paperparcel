046fadd91651ae0c1828f25927f61c1867359c1d
Bradley Campbell <grandstaish@gmail.com>
2017-04-02 11:41:31 +1200

    Remove kapt checks. Fixes build when using kotlin 1.1.2.

--- paperparcel-compiler/src/main/java/paperparcel/PaperParcelProcessor.java
+++ paperparcel-compiler/src/main/java/paperparcel/PaperParcelProcessor.java
@@ -24,13 +24,8 @@ import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.Processor;
 import javax.lang.model.SourceVersion;
-import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.TypeParameterElement;
-import javax.lang.model.type.DeclaredType;
-import javax.lang.model.type.TypeKind;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
-import javax.tools.Diagnostic;
 
 /**
  * The annotation processor responsible for generating the classes that drive the PaperParcel
@@ -38,10 +33,6 @@ import javax.tools.Diagnostic;
  */
 @AutoService(Processor.class)
 public class PaperParcelProcessor extends BasicAnnotationProcessor {
-  private static final String KAPT1_GENERATED_OPTION = "kapt.kotlin.generated";
-  private static final String KAPT2_PROCESSING_ENVIRONMENT =
-      "org.jetbrains.kotlin.annotation.processing.impl.KotlinProcessingEnvironment";
-
   @Override public SourceVersion getSupportedSourceVersion() {
     return SourceVersion.latestSupported();
   }
@@ -51,10 +42,6 @@ public class PaperParcelProcessor extends BasicAnnotationProcessor {
     Types types = processingEnv.getTypeUtils();
     Elements elements = processingEnv.getElementUtils();
     Filer filer = new FormattingFiler(processingEnv.getFiler());
-
-    if (!checkKaptVersion(messager)) {
-      return ImmutableList.of();
-    }
 
     AdapterRegistry adapterRegistry = new AdapterRegistry(elements, types);
     OptionsHolder optionsHolder = new OptionsHolder();
@@ -88,36 +75,5 @@ public class PaperParcelProcessor extends BasicAnnotationProcessor {
             paperParcelValidator,
             paperParcelDescriptorFactory,
             paperParcelGenerator));
-  }
-
-  private boolean checkKaptVersion(Messager messager) {
-    // Older kapt versions had a lot of bugs that prevent PaperParcel from working. Try to
-    // detect these here and show a helpful message to the user informing them they need to
-    // upgrade their kotlin version.
-    boolean kapt1 = processingEnv.getOptions().get(KAPT1_GENERATED_OPTION) != null;
-    boolean kapt2 = processingEnv.getClass().getName().equals(KAPT2_PROCESSING_ENVIRONMENT);
-    if (kapt1) {
-      messager.printMessage(Diagnostic.Kind.ERROR, ErrorMessages.KAPT1_INCOMPATIBLE);
-      return false;
-    }
-    if (kapt2) {
-      if (hasKt13804()) {
-        messager.printMessage(Diagnostic.Kind.ERROR, ErrorMessages.KAPT2_KT_13804);
-        return false;
-      } else {
-        messager.printMessage(Diagnostic.Kind.WARNING, ErrorMessages.KAPT2_UNSTABLE_WARNING);
-      }
-    }
-    return true;
-  }
-
-  private boolean hasKt13804() {
-    // Checks for KT-13804. This affects only kotlin 1.0.4.
-    Elements elements = processingEnv.getElementUtils();
-    Types types = processingEnv.getTypeUtils();
-    TypeElement comparable = elements.getTypeElement("java.lang.Comparable");
-    TypeParameterElement comparableParam = comparable.getTypeParameters().get(0);
-    DeclaredType integerType = (DeclaredType) elements.getTypeElement("java.lang.Integer").asType();
-    return types.asMemberOf(integerType, comparableParam).getKind() == TypeKind.TYPEVAR;
   }
 }
